3b771862bc126f887a8ee00deec65bb6e2fb532b
Balves129 <b.balves129@gmail.com>
2022-05-31 17:14:04 -0300

    master

---
+++ qqcoisa.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+
+def somar():
+    try:
+        label3['text']= int(entry1.get())+ int(entry2.get())
+        label3['foreground']= 'Blue'
+    except:
+        label3['text']= 'Não válido'
+        label3['foreground'] = 'Red'
+
+janela= Tk()
+janela.geometry('800x700+200+500')
+janela.config(bg="white")
+janela.minsize(width= 200, height= 200)
+janela.maxsize(width= 2000, height= 2000)
+
+label3 = Label(janela, text="",
+               font = "Arial 20 ",bg="white", foreground="blue")
+
+label1 = Label(janela, text="AULÃO DO BRENO 2022",
+               font = "Algerian 47 ", bg="white",foreground="Blue")
+label2= Label(janela, text="+",font="Arial 20", foreground="blue" , bg='white')
+entry1= Entry(janela, font= "Rockwell 20")
+entry2= Entry(janela, font= "Rockwell 20")
+btn1 = Button(janela, text="somar", font='Rockwell 40', foreground= "blue", bg='white',command=somar)
+
+
+label1.pack(side=TOP)
+label3.pack(side=TOP)
+entry1.pack(side=TOP)
+label2.pack(side=TOP)
+entry2.pack(side=TOP)
+btn1.pack(side= TOP)
+
+janela.mainloop()
